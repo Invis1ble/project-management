cd7e46983b394fadbdd8f7a2e2a0c1ef8ba6ce6e
Invis1ble <Invis1ble@users.noreply.github.com>
2024-06-06 21:44:14 +0300

    Improve test.

--- tests/HotfixPublication/Application/UseCase/Command/CreateHotfixPublication/CreateHotfixPublicationCommandHandlerTest.php
+++ tests/HotfixPublication/Application/UseCase/Command/CreateHotfixPublication/CreateHotfixPublicationCommandHandlerTest.php
@@ -7,21 +7,19 @@ namespace HotfixPublication\Application\UseCase\Command\CreateHotfixPublication;
 use Invis1ble\Messenger\Command\TraceableCommandBus;
 use Invis1ble\Messenger\Event\TraceableEventBus;
 use Invis1ble\ProjectManagement\HotfixPublication\Application\UseCase\Command\CreateHotfixPublication\CreateHotfixPublicationCommand;
+use Invis1ble\ProjectManagement\HotfixPublication\Application\UseCase\Command\ProceedToNextStatus\ProceedToNextStatusCommand;
 use Invis1ble\ProjectManagement\HotfixPublication\Domain\Event\HotfixPublicationCreated;
 use Invis1ble\ProjectManagement\HotfixPublication\Domain\Model\Status\StatusCreated;
 use Invis1ble\ProjectManagement\HotfixPublication\Domain\Repository\HotfixPublicationRepositoryInterface;
-use Invis1ble\ProjectManagement\Shared\Domain\Model\ContinuousIntegration\Project;
-use Invis1ble\ProjectManagement\Shared\Domain\Model\DevelopmentCollaboration\MergeRequest;
-use Invis1ble\ProjectManagement\Shared\Domain\Model\SourceCodeRepository\Branch;
 use Invis1ble\ProjectManagement\Shared\Domain\Model\SourceCodeRepository\Tag;
-use Invis1ble\ProjectManagement\Shared\Domain\Model\TaskTracker\Board\BoardId;
-use Invis1ble\ProjectManagement\Shared\Domain\Model\TaskTracker\Issue;
-use Invis1ble\ProjectManagement\Shared\Domain\Model\TaskTracker\Sprint;
+use Invis1ble\ProjectManagement\Tests\Shared\Infrastructure\Domain\Model\TaskTracker\Issue\CreateIssuesTrait;
 use Psr\Http\Message\UriFactoryInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class CreateHotfixPublicationCommandHandlerTest extends KernelTestCase
 {
+    use CreateIssuesTrait;
+
     public function testCreateHotfixPublication(): void
     {
         self::bootKernel();
@@ -40,58 +38,16 @@ class CreateHotfixPublicationCommandHandlerTest extends KernelTestCase
         /** @var TraceableEventBus $eventBus */
         $eventBus = $container->get(TraceableEventBus::class);
 
-        $mergeRequests = new MergeRequest\MergeRequestList(
-            new MergeRequest\MergeRequest(
-                id: MergeRequest\MergeRequestId::from(4),
-                title: MergeRequest\Title::fromString('Update issue branch'),
-                projectId: Project\ProjectId::from(5),
-                projectName: Project\Name::fromString('PROJECT'),
-                sourceBranchName: Branch\Name::fromString('master'),
-                targetBranchName: Branch\Name::fromString('PROJECT-2'),
-                status: MergeRequest\Status::Merged,
-                guiUrl: $uriFactory->createUri('https://gitlab.example.com/example/coolproject/-/merge_requests/4'),
-                details: new MergeRequest\Details\Details(
-                    status: new MergeRequest\Details\Status\StatusNotOpen(),
-                ),
-            ),
+        $hotfixes = $this->createIssues(
+            uriFactory: $uriFactory,
         );
 
-        $mergeRequestsToMerge = $mergeRequests->append(
-            new MergeRequest\MergeRequest(
-                id: MergeRequest\MergeRequestId::from(45),
-                title: MergeRequest\Title::fromString('Close PROJECT-2 Fix terrible bug'),
-                projectId: Project\ProjectId::from(5),
-                projectName: Project\Name::fromString('PROJECT'),
-                sourceBranchName: Branch\Name::fromString('PROJECT-2'),
-                targetBranchName: Branch\Name::fromString('master'),
-                status: MergeRequest\Status::Open,
-                guiUrl: $uriFactory->createUri('https://gitlab.example.com/example/coolproject/-/merge_requests/45'),
-                details: new MergeRequest\Details\Details(
-                    status: new MergeRequest\Details\Status\StatusMergeable(),
-                ),
-            ),
-        );
+        $hotfixesArray = $hotfixes->toArray();
 
         $command = new CreateHotfixPublicationCommand(
             tagName: Tag\VersionName::create(),
-            tagMessage: Tag\Message::fromString('Fix terrible bug | PROJECT-2'),
-            hotfixes: new Issue\IssueList(
-                new Issue\Issue(
-                    id: Issue\IssueId::from(1),
-                    key: Issue\Key::fromString('PROJECT-2'),
-                    typeId: Issue\TypeId::fromString('3'),
-                    summary: Issue\Summary::fromString('Fix terrible bug'),
-                    sprints: new Sprint\SprintList(
-                        new Sprint\Sprint(
-                            boardId: BoardId::from(42),
-                            name: Sprint\Name::fromString('June 2024 1-2'),
-                            state: Sprint\State::Active,
-                        ),
-                    ),
-                    mergeRequests: $mergeRequests,
-                    mergeRequestsToMerge: $mergeRequestsToMerge,
-                ),
-            ),
+            tagMessage: Tag\Message::fromString("{$hotfixesArray[0]->summary} | {$hotfixesArray[0]->key}"),
+            hotfixes: $hotfixes,
         );
 
         $commandBus->dispatch($command);
@@ -99,15 +55,26 @@ class CreateHotfixPublicationCommandHandlerTest extends KernelTestCase
         $dispatchedEvents = $eventBus->getDispatchedEvents();
 
         $this->assertCount(1, $dispatchedEvents);
-        $this->assertArrayHasKey(0, $dispatchedEvents);
-        $dispatchedEvent = $dispatchedEvents[0]->event;
-        $this->assertInstanceOf(HotfixPublicationCreated::class, $dispatchedEvent);
-        $this->assertObjectEquals(new StatusCreated(), $dispatchedEvent->status);
 
-        $publication = $hotfixPublicationRepository->get($dispatchedEvent->id);
+        $dispatchedCommands = $commandBus->getDispatchedCommands();
+
+        $this->assertCount(2, $dispatchedCommands);
+
+        $this->assertArrayHasKey(0, $dispatchedEvents);
+        $event0 = $dispatchedEvents[0]->event;
+        $this->assertInstanceOf(HotfixPublicationCreated::class, $event0);
+        $this->assertObjectEquals(new StatusCreated(), $event0->status);
+        $this->assertObjectEquals($command->hotfixes, $event0->hotfixes);
+
+        $publication = $hotfixPublicationRepository->get($event0->id);
 
         $this->assertObjectEquals($command->tagName, $publication->tagName());
         $this->assertObjectEquals($command->tagMessage, $publication->tagMessage());
         $this->assertObjectEquals($command->hotfixes, $publication->hotfixes());
+
+        $this->assertArrayHasKey(0, $dispatchedCommands);
+        $command0 = $dispatchedCommands[0]->command;
+        $this->assertInstanceOf(ProceedToNextStatusCommand::class, $command0);
+        $this->assertObjectEquals($publication->id(), $command0->id);
     }
 }
