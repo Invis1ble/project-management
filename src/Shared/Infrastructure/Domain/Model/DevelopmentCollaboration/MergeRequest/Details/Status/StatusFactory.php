0574ae3c1b61033d2221c8555b19c1e954743764
Invis1ble <Invis1ble@users.noreply.github.com>
2024-06-21 23:43:28 +0300

    Fix Coding Standards violation.

--- src/Shared/Infrastructure/Domain/Model/DevelopmentCollaboration/MergeRequest/Details/Status/StatusFactory.php
+++ src/Shared/Infrastructure/Domain/Model/DevelopmentCollaboration/MergeRequest/Details/Status/StatusFactory.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Invis1ble\ProjectManagement\Shared\Infrastructure\Domain\Model\DevelopmentCollaboration\MergeRequest\Details\Status;
 
 use Invis1ble\ProjectManagement\Shared\Domain\Model\DevelopmentCollaboration\MergeRequest\Details\Status\Dictionary;
+use Invis1ble\ProjectManagement\Shared\Domain\Model\DevelopmentCollaboration\MergeRequest\Details\Status\StatusFactory as StaticStatusFactory;
 use Invis1ble\ProjectManagement\Shared\Domain\Model\DevelopmentCollaboration\MergeRequest\Details\Status\StatusFactoryInterface;
 use Invis1ble\ProjectManagement\Shared\Domain\Model\DevelopmentCollaboration\MergeRequest\Details\Status\StatusInterface;
-use Invis1ble\ProjectManagement\Shared\Domain\Model\DevelopmentCollaboration\MergeRequest\Details\Status\StatusFactory as StaticStatusFactory;
 
 final readonly class StatusFactory implements StatusFactoryInterface
 {
