05ca3240d4996c25a2503745f4c34472d2372ab9
Invis1ble <Invis1ble@users.noreply.github.com>
2025-02-14 01:19:29 +0300

    Fix CS violation.

--- tests/Shared/Infrastructure/Domain/Serializer/Normalizer/ListNormalizer/ListNormalizerTest.php
+++ tests/Shared/Infrastructure/Domain/Serializer/Normalizer/ListNormalizer/ListNormalizerTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Invis1ble\ProjectManagement\Tests\Shared\Infrastructure\Domain\Serializer\Normalizer\ListNormalizer;
 
-use Invis1ble\ProjectManagement\Tests\Shared\SerializerAwareTestCase;
 use Invis1ble\ProjectManagement\Tests\Shared\Infrastructure\Domain\Serializer\Normalizer\IdNormalizer\TestId;
+use Invis1ble\ProjectManagement\Tests\Shared\SerializerAwareTestCase;
 
 class ListNormalizerTest extends SerializerAwareTestCase
 {
