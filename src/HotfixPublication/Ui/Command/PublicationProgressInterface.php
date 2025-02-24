fe4faa9320cfdc682307d61f8ed004a5591d03db
Invis1ble <Invis1ble@users.noreply.github.com>
2025-02-24 23:03:59 +0300

    Extract methods into PublicationProgressInterface.

--- src/HotfixPublication/Ui/Command/PublicationProgressInterface.php
+++ src/HotfixPublication/Ui/Command/PublicationProgressInterface.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace Invis1ble\ProjectManagement\HotfixPublication\Ui\Command;
 
+use Invis1ble\Messenger\Event\EventInterface;
+
 interface PublicationProgressInterface
 {
+    public function start(string $status = 'inited'): void;
 
+    public function advance(int $step = 1): void;
+
+    public function finish(): void;
+
+    public function setStatus(string $status, bool $display = true): void;
+
+    public function addEvent(EventInterface $event): void;
 }
